50af7f66808264008b33cb15265560d884ca32a6
Suraj Mandal <suraj.mandal@cityexperiences.com>
2023-10-25 01:00:14 +0530

    adding cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
-
+const cors = require('cors');
 const { getPosts, getPost, addPost } = require('./database.js')
 
 const app = express()
+app.use(cors());
 
 app.use(express.json())
 
